7308bd3cc190bd3d393f483cf20650211760b327
wisdomphiri408 <wisdomphiri408@gmail.com>
2025-11-24 11:51:21 +0200

    Our first custom component

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,42 +1,16 @@
 import products from "@/assets/data/products";
-import Colors from "@/src/constants/Colors";
-import { Image, StyleSheet, Text, View } from "react-native";
+import ProductListItem from "@/src/components/ProductListItem";
+import { View } from "react-native";
 
-const product = products[1];
-
-
-export default function Index() {
+export default function MenuScreen() {
   return (
-    <View
-      style={styles.container}
-    >
-      <Image 
-        source={{uri: product.image}}
-        style={styles.image}
-      />
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price}</Text>
+    <View>
+      <ProductListItem product={products[0]} />
+      <ProductListItem product = {products[1]} />
+      <ProductListItem product={products[2]} />
+      <ProductListItem product = {products[3]} />
+      <ProductListItem product={products[4]} />
+      <ProductListItem product = {products[5]} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "white",
-    padding: 10,
-    borderRadius: 20,
-  },
-  image: {
-    width: "100%",
-    aspectRatio: 1,
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: '600',
-    marginVertical: 10,
-  },
-  price: {
-    color: Colors.light.tint,
-    fontWeight: "bold",
-  }
-})
